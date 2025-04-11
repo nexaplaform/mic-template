62e616e0a7ab4dbc6339677ecc582038a91411c3
Jesis Finol <jfinol02@gmail.com>
2025-04-11 22:07:48 +0200

    Initial commit

---
+++ src/main/java/com/nexaplatform/MicTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.nexaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexaplatform/MicTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
